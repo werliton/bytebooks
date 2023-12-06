88e57a42f6530b1ee1784e885003bd749175a215
Pedro Mello <contato@cicatriz.dev>
2023-12-06 18:25:45 -0300

    codigo da aula 1.3 do curso 2

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,18 @@
 import { Route, Switch } from 'react-router-dom';
 import Catalog from '../pages/Catalog';
 import BookDetail from '../pages/BookDetail';
+import { Provider } from 'react-redux';
+import { store } from '../store/store';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export const Routes = () => (
-	<Switch>
-		<Route exact path='/' component={Catalog} />
-		<Route path='/book' component={BookDetail} />
-	</Switch>
+	<Provider store={store}>
+		<Header />
+		<Switch>
+			<Route exact path='/' component={Catalog} />
+			<Route path='/book' component={BookDetail} />
+		</Switch>
+		<Footer />
+	</Provider>
 );
