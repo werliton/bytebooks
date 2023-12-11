4e2e0884c9b2b0a4ea28e2039d4be57cfd3ad512
Pedro Mello <contato@cicatriz.dev>
2023-12-11 17:52:53 -0300

    codigo da aula 5.1 do curso 2

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,8 +1,20 @@
+import PageSection from '../../components/PageSection';
+
 const Order = () => {
+	const isLoading = true;
 	return (
-		<div>
-			<h1>Order</h1>
-		</div>
+		<>
+			<PageSection>
+				<h1 className='text-2xl md:text-4xl text-white font-bold my-5'>Finalizar compra</h1>
+			</PageSection>
+			<div className='mt-20 w-full items-center justify-center flex'>
+				{isLoading ? (
+					<img src='/loading.gif' alt='carregando' width={200} />
+				) : (
+					<img src='/pedido_confirmado.webp' alt='pedido confirmado' />
+				)}
+			</div>
+		</>
 	);
 };
 
