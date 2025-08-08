10ddbf5b80ea93891e70500247b6021a5ff5574a
Werliton Silva <werlitoncarlos@gmail.com>
2025-08-08 09:11:25 -0300

    feat: setting up Error Boundary

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,23 @@
-import { Route, Switch } from 'react-router-dom';
-import Catalog from '../pages/Catalog';
-import BookDetail from '../pages/BookDetail';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import { AppContext } from '../store/app';
-import Order from '../pages/Order';
+import { Route, Switch } from "react-router-dom";
+import Catalog from "../pages/Catalog";
+import BookDetail from "../pages/BookDetail";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { AppContext } from "../store/app";
+import Order from "../pages/Order";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 export const Routes = () => (
-	<AppContext>
-		<Header />
-		<Switch>
-			<Route exact path='/' component={Catalog} />
-			<Route path='/book' component={BookDetail} />
-			<Route path='/order' component={Order} />
-			<Route path='*' component={() => <h1>Não ha nada aqui.</h1>} />
-		</Switch>
-		<Footer />
-	</AppContext>
+  <ErrorBoundary>
+    <AppContext>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Catalog} />
+        <Route path="/book" component={BookDetail} />
+        <Route path="/order" component={Order} />
+        <Route path="*" component={() => <h1>Não ha nada aqui.</h1>} />
+      </Switch>
+      <Footer />
+    </AppContext>
+  </ErrorBoundary>
 );
